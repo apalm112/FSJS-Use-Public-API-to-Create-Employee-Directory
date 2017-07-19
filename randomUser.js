a9898ccca88af63e750344323895acc3dd27426a
sofa_king <apalm112@gmail.com>
2017-07-19 15:10:06 -0700

    Added initial HTML structure w/ script & CSS links.  TODO's for project in randomUser.js

---
+++ randomUser.js
@@ -0,0 +1,38 @@
+// 	Review the Random User Generator documentation. Grab the example they provide and console.log the data so you can see what information you’ll receive and start to think about how you’ll access the correct information and display it on the page.
+
+
+/*	Project Instructions:
+			Get and display 12 random users from The Random User Generator API
+			Using photos and information that the API provides, you’ll display 12 users, along with some basic information:
+			Image
+			First and Last Name
+			Username
+			City & Country
+
+			Create a modal window that will pop up when any part of the user’s row is clicked. The following details should display in the modal window:
+			Image
+			Name
+			Username
+			Email
+			Cell Number
+			Detailed Address, including street name and number, city, country and post code.
+			Birthdate
+			There is a way to close the modal window
+
+			Structure and style the user directory so that it roughly matches the provide mockup.
+			Display the users in a grid or table
+			Add a hover state to the rows of the user table.
+			Make sure there’s a way to close the modal window
+
+			PUSH GOALS:
+			TODO: Employees can be filtered by name or username
+			Add a way to filter the directory by name or username.
+
+			To do this, you’ll need to request a random user nationality that will only return data in the English alphabet. Note: you don't have to rely on the API to return search results. You'll need to write functionality that filters results once they already on the page.
+
+			TODO:	Functionality has been added to switch back and forth between employees when the detail modal window is open.
+
+			Add a way to move back and forth between employee detail windows when the modal window is open.
+
+
+*/
