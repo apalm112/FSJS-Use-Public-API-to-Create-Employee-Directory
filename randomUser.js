a239a024999b02e12faf5e68679297ad7468aad6
sofa_king <apalm112@gmail.com>
2017-07-25 12:39:36 -0700

    Added icomoon icons & positioned them on the modal for UI.  Need JS to wire it up now.

--- randomUser.js
+++ randomUser.js
@@ -87,7 +87,9 @@ let cloud = {};
 		modalHTML += '<ul>';
 		modalHTML += '<li><img src=' + cloud.results[targetElement].picture.medium + '></li>';
 		modalHTML += '<li>' + cloud.results[targetElement].name.first + ' ' +  cloud.results[targetElement].name.last + '</li>';
+		modalHTML += '<span class="icon-circle-left"></span>';
 		modalHTML += '<li>' + cloud.results[targetElement].login.username + '</li>';
+		modalHTML += '<span class="icon-circle-right"></span>';
 		modalHTML += '<li>' + cloud.results[targetElement].email + '</li>';
 		modalHTML += '<li class="line">---------------------------------------------------------</li>';
 
@@ -139,11 +141,19 @@ let cloud = {};
 		);
 	}
 
+	/* ********************************************************/
+	function modalSwitch() {
+	/* TODO:	Functionality added to switch back and forth between employees when the detail modal window is open. */
+
+
+
+
+	}	//	end modalSwitch()
+	/****************************************************************************/
 })(window);
 
-/* *********************************************************
 
-			Project Instructions:
+			/* Project Instructions:
 				Structure and style the user directory so that it roughly matches the provide mockup.
 				Display the users in a grid or table
 ------->Add a hover state to the rows of the user table.	<--------------
@@ -154,7 +164,5 @@ let cloud = {};
 
 				To do this, you’ll need to request a random user nationality that will only return data in the English alphabet. Note: you don't have to rely on the API to return search results. You'll need to write functionality that filters results once they already on the page.
 
-				TODO:	Functionality has been added to switch back and forth between employees when the detail modal window is open.
 
-				Add a way to move back and forth between employee detail windows when the modal window is open.
-				****************************************************************************/
+				Add a way to move back and forth between employee detail windows when the modal window is open. */
